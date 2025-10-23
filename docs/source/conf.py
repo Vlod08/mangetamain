d43bf61733b13281f74396f707ba21f4025d98ef
Bryan Ly <bryan29.ly@gmail.com>
2025-10-23 17:35:47 +0200

    add extension sphinx autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, os.path.abspath('../../src/mangetamain'))
+
 project = 'mangetamain'
 copyright = '2025, Bryan Ly, Lokesh'
 author = 'Lina Hazime, Bryan LY, Khalil OUNIS, Mohammed ELAMINE, Lokeshwaran VENGADABADY'
@@ -13,7 +19,7 @@ author = 'Lina Hazime, Bryan LY, Khalil OUNIS, Mohammed ELAMINE, Lokeshwaran VEN
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo']
 
 templates_path = ['_templates']
 exclude_patterns = []
--- src/mangetamain/main.py
+++ src/mangetamain/main.py
@@ -1,8 +1,11 @@
-import streamlit as st 
+import streamlit as st
 
 
 def main():
+    """test docstring
+    """
     st.write("Launched application !")
+
 
 if __name__ == "__main__":
     main()
